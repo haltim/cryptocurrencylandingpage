97ae586bc27ce3d6993e1f23edc4883c9ec0c900
haltim <timoayomide2000@gmail.com>
2022-05-28 17:01:37 +0100

    Updates

---
+++ precache-manifest.9bbf8ac3ab9f614ff595fdfca3f073be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "491dd48a0a01b016fab1",
+    "url": "/cryptocurrencylandingpage/static/css/main.294cc761.chunk.css"
+  },
+  {
+    "revision": "491dd48a0a01b016fab1",
+    "url": "/cryptocurrencylandingpage/static/js/main.491dd48a.chunk.js"
+  },
+  {
+    "revision": "e1960fff7d1dd35ddfb0",
+    "url": "/cryptocurrencylandingpage/static/js/1.e1960fff.chunk.js"
+  },
+  {
+    "revision": "acc0907d1f406b9331fc",
+    "url": "/cryptocurrencylandingpage/static/js/runtime~main.acc0907d.js"
+  },
+  {
+    "revision": "5b6ea36acd204262af4d84e35ca4b8e8",
+    "url": "/cryptocurrencylandingpage/static/media/hero-img.5b6ea36a.png"
+  },
+  {
+    "revision": "583edf6ba94fb29bbfd3c87bc479f27b",
+    "url": "/cryptocurrencylandingpage/static/media/trade.583edf6b.png"
+  },
+  {
+    "revision": "fa6f9227242d063badcffcf7e4de3d6a",
+    "url": "/cryptocurrencylandingpage/index.html"
+  }
+];
